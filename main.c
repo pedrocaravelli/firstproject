fb1a752faabe365773d3923e3a8bec0a005a512f
pedro <pedrocaravelli02@gmail.com>
2018-04-26 13:36:21 -0300

    hola

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+int resta(int a, int b) {
+	return a-b ;
+
+}
+
+int suma(int a, int b) {
+	return a+b ;
+
+}
+
+int multi(int a, int b) {
+	return a*b ;
+
+}
+
+int sumatory(int a){
+	int i;
+	int rta=0;
+	for(i=0; i <= a; i=i+1){
+		rta=rta + i;
+	}
+	return rta;
+
+}
+
+int multiplicatory(int a){
+	int i;
+	int rta=1;
+	for(i=1; i <= a; i=i+1){
+		rta=rta * i;
+	}
+	return rta;
+
+}
+
+int Fibonacci(int a){
+	int orden;
+	int rta=0
+	int anterior=
+	for(orden=0; i <= )
+
+
+
+}
+
+int main () {
+	printf("Hello World\n");
+	printf("%i\n", suma(11,34));
+	printf("%i\n", resta(5, 8));
+	printf("%i\n", multi(1480,5489));
+	printf("%i\n", sumatory(5));
+	printf("%i\n", multiplicatory(5));
+	return 0;
+}
